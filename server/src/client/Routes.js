0473223a3ec2b04e3a2ff51710c41c0d4f4d9cdd
Nicholas Marcaccini <nicholasmarca@gmail.com>
2018-09-03 07:49:47 +0100

    first commit;

---
+++ server/src/client/Routes.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import HomePage from './pages/HomePage';
+import UsersListPage from './pages/UsersListPage';
+import App from './App';
+import NotFoundPage from "./pages/NotFoundPage";
+import AdminsListPage from "./pages/AdminsListPage";
+
+export default [
+  {
+    ...App,
+    routes: [
+      {
+        ...HomePage,
+        path: '/',
+        exact: true,
+      },
+      {
+        ...UsersListPage,
+        path: '/users',
+      },
+      {
+        ...AdminsListPage,
+        path: '/admins'
+      },
+      {
+        ...NotFoundPage
+      }
+    ]
+  }
+];
---
+++ server/src/client/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return(
+    <div className="center-align" style={{marginTop: 200}}>
+     <h3>Welcome</h3>
+      <p>Checkout out these awesome features</p>
+    </div>
+  );
+};
+
+export default {
+  component: Home
+};
